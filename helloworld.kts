1b36688850e651694bcadd93f01ab046824d9656
adhi <adhiyamansisubalan@gmail.com>
2024-10-26 20:55:17 +0800

    Adding Kotlin Scripts

--- helloworld.kts
+++ helloworld.kts
@@ -1,7 +1,12 @@
 
 class HelloWorld{
+
+    val actions = arrayOf("i","am", "Ironman")
     fun printHelloWorld() : String{
         return "Hello World!!!"
+    }
+    fun getList() :  Array<String>{
+        return actions
     }
 }
 
--- src/main/java/com/learning/org/springboot/MicroserviceApplication.java
+++ src/main/java/com/learning/org/springboot/MicroserviceApplication.java
@@ -1,25 +1,23 @@
 package com.learning.org.springboot;
 
 import kotlin.script.experimental.jsr223.KotlinJsr223DefaultScriptEngineFactory;
-import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import java.io.File;
 import java.io.FileReader;
-import java.io.Reader;
+
 
 @SpringBootApplication
 public class MicroserviceApplication implements CommandLineRunner {
 
-
-@Autowired
+	@Autowired
 	private ConfigurableApplicationContext configurableApplicationContext;
 
 	public static void main(String[] args) {
@@ -31,11 +29,16 @@ public class MicroserviceApplication implements CommandLineRunner {
 		var engine = new KotlinJsr223DefaultScriptEngineFactory().getScriptEngine();
 		var file = new File("./helloworld.kts");
 		var obj = engine.eval(new FileReader(file));
-		configurableApplicationContext.getBeanFactory()
-				.registerSingleton("helloWorld", obj);
+		configurableApplicationContext.getBeanFactory().registerSingleton("helloWorld", obj);
 
-		var instanceBean = configurableApplicationContext.getBean("helloWorld") ;
+		var instanceBean = configurableApplicationContext.getBean("helloWorld");
 		var invokedValue = instanceBean.getClass().getMethod("printHelloWorld").invoke(instanceBean);
 		System.out.println(invokedValue);
+
+		var list =  instanceBean.getClass().getMethod("getList").invoke(instanceBean);
+		for (var str : (String[])list){
+			System.out.println(str);
+		}
+
 	}
 }
